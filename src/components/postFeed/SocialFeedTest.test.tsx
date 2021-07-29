90df7e5e26fc9cdd4a1bc565a4709f53683e831f
Brandon Kirsch <brandon.kirsch.4@gmail.com>
2021-07-29 08:49:43 -0700

    A test
    Added one test that doesn't even touch "testable" code but at least it passes

--- src/components/postFeed/SocialFeedTest.test.tsx
+++ src/components/postFeed/SocialFeedTest.test.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
-import PostList from './postList';
+import PostFlatList from './postFlatList';
 import renderer from 'react-test-renderer';
+import { CognitoAccessToken } from 'amazon-cognito-identity-js';
 
 const testData = [
-    {postId: 'JQWERY', user: 'Test User', body: 'I wanted to do a test so I entered test data into the test data constant in the test post file so that the text I typed would appear in the tested text test.'}
+    {post_id: 'JQWERY', username: 'Test User', post_text: 'I wanted to do a test so I entered test data into the test data constant in the test post file so that the text I typed would appear in the tested text test.'}
 ]
 
 describe('<PostList />', () => {
+    const list = renderer.create(<PostFlatList data={testData}/>)
 
+    it('should display username correctly', () => {
+        const jason = JSON.stringify(list.toJSON());
+        const actual = JSON.parse(jason);
+
+        expect(actual.children[0].props.data[0]).toHaveProperty('username', 'Test User');
+    })
 })
